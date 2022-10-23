e67b97dd6110c3552d62e10cb5db6027fa8d8584
Tatyana Larina <jerrymurr@ya.ru>
2022-10-23 16:14:22 +0300

    commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,61 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public int getSum(int[] stats) {
+        int sum = 0;
+        for (int s : stats) {
+            sum += s;
+        }
+        return sum;
+    }
+
+    public float getAverage(int[] stats) {
+        int sum = getSum(stats);
+        float average = sum/stats.length;
+        return average;
+    }
+
+    public int getMaxMonth(int[] stats) {
+        int maxMonth = 0;
+        for (int i = 0; i < stats.length; i++) {
+            if (stats[i] >= stats[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+        return maxMonth+1;
+    }
+
+    public int getMinMonth(int[] stats) {
+        int minMonth = 0;
+        for (int i = 0; i < stats.length; i++) {
+            if (stats[i] <= stats[minMonth]) {
+                minMonth = i;
+            }
+        }
+        return minMonth+1;
+    }
+
+    public int getLessAverage(int[] stats) {
+        int count = 0;
+        float average = getAverage(stats);
+        for (int s : stats) {
+            if (s < average) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+    public int getMoreAverage(int[] stats) {
+        int count = 0;
+        float average = getAverage(stats);
+        for (int s : stats) {
+            if (s > average) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,80 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+    @Test
+    public void getSum() {
+        StatsService service = new StatsService();
+
+        int stats[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int exceptedSum = 180;
+        int actualSum = service.getSum(stats);
+
+        Assertions.assertEquals(exceptedSum, actualSum);
+    }
+
+    @Test
+    public void getAverage() {
+        StatsService service = new StatsService();
+
+        int stats[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        float exceptedAverage = 15;
+        float actualAverage = service.getAverage(stats);
+
+        Assertions.assertEquals(exceptedAverage, actualAverage);
+
+    }
+
+    @Test
+    public void getMaxMonth() {
+        StatsService service = new StatsService();
+
+        int stats[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int exceptedMaxMonth = 8;
+        int actualMaxMonth = service.getMaxMonth(stats);
+
+        Assertions.assertEquals(exceptedMaxMonth, actualMaxMonth);
+    }
+
+    @Test
+    public void getMinMonth() {
+        StatsService service = new StatsService();
+
+        int stats[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int exceptedMinMonth = 9;
+        int actualMinMonth = service.getMinMonth(stats);
+
+        Assertions.assertEquals(exceptedMinMonth, actualMinMonth);
+    }
+
+    @Test
+    public void getLessAverage() {
+        StatsService service = new StatsService();
+
+        int stats[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int exceptedLessAverage = 5;
+        int actualLessAverage = service.getLessAverage(stats);
+
+        Assertions.assertEquals(exceptedLessAverage, actualLessAverage);
+    }
+
+    @Test
+    public void getMoreAverage() {
+        StatsService service = new StatsService();
+
+        int stats[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int exceptedMoreAverage = 5;
+        int actualMoreAverage = service.getMoreAverage(stats);
+
+        Assertions.assertEquals(exceptedMoreAverage, actualMoreAverage);
+    }
+
+}
